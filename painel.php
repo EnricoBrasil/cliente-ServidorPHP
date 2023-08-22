5a0ffd249ca1ddcd10d6dc3b3f7f5e465db2f90b
EnricoBrasil <cylodabiliro@hotmail.com>
2023-08-22 16:44:03 -0300

    feat: login

---
+++ painel.php
@@ -0,0 +1,21 @@
+<?php
+
+include('protect.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Painel</title>
+</head>
+<body>
+    Bem Vindo ao Painel, <?php echo $_SESSION['name']; ?>
+
+    <p>
+
+        <a href="logout.php">Sair</a>
+    </p>
+</body>
+</html>
---
+++ protect.php
@@ -0,0 +1,13 @@
+<?php
+
+if(!isset($_SESSION)) {
+    session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+    die("Voce nao pode acessar essa pagina porque nao está logado. <p><a href=\"index.php\">Entrar</p>");
+}
+
+
+
+?>
