cdf530978cc666703f0815e93b7c2ad8892f4346
Enrico Moraes Brasil Mustafa <enrico46444506@aluno.ba.senac.br>
2023-08-22 11:35:58 -0300

    feat:html Login

--- index.php
+++ index.php
@@ -6,6 +6,21 @@
     <title>Login | Seja Bem-Vindo</title>
 </head>
 <body>
+    <h1>Login</h1>
+    <form action="" method = "POST">
+        <p>
+            <label for="">email</label>
+            <input type="text" name="email">
+        </p>
+        <p>
+            <label for="">email</label>
+            <input type="password" name="senha">
+        </p>
+        <p>
+            <button type="submit">Entrar</button>
+        </p>
+    </form>
+
     
 </body>
 </html>
