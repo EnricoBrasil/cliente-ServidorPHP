ab0f97691b0f9d4ef4cab6f943fb5f842bf86185
fabionascimentodev <fabio.fbn1@gmail.com>
2023-08-22 21:13:58 -0300

    style: index.php, painel.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 include('controller/conexao.php');
-if(isset($_POST['email']) || isset($_POST['senha'])) {
-    if(strlen($_POST['email']) == 0) {
+if (isset($_POST['email']) || isset($_POST['senha'])) {
+    if (strlen($_POST['email']) == 0) {
         echo "Preencha seu e-mail";
-    } else if(strlen($_POST['senha']) == 0) {
+    } else if (strlen($_POST['senha']) == 0) {
         echo "Preencha sua senha";
-    } else{
+    } else {
 
         $email = $mysqli->real_escape_string($_POST['email']);
         $senha = $mysqli->real_escape_string($_POST['senha']);
@@ -14,11 +14,11 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
         $sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
 
         $quantidade = $sql_query->num_rows;
-        if($quantidade == 1){
+        if ($quantidade == 1) {
 
             $usuario = $sql_query->fetch_assoc();
 
-            if(!isset($_SESSION)) {
+            if (!isset($_SESSION)) {
                 session_start();
             }
 
@@ -26,8 +26,7 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
             $_SESSION['name'] = $usuario['nome'];
 
             header("Location: View/painel.php");
-
-        }else{
+        } else {
             echo "Falha ao logar! E-mail ou senha incorretos";
         }
     }
@@ -35,27 +34,42 @@ if(isset($_POST['email']) || isset($_POST['senha'])) {
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="View/css/style.css">
     <title>Login | Seja Bem-Vindo</title>
 </head>
-<body>
-    <h1>Login</h1>
-    <form action="" method = "POST">
-        <p>
-            <label for="">email:</label>
-            <input type="text" name="email">
-        </p>
-        <p>
-            <label for="">Senha:</label>
-            <input type="password" name="senha">
-        </p>
-        <p>
-            <button type="submit">Entrar</button>
-        </p>
-    </form>
 
-    
+<body>
+    <div class="screen">
+
+        <div class="login-section">
+            <div class="login-content">
+
+                <div class="access">
+                    <h1>Login</h1>
+                    <p>Seja Bem Vindo</p>
+                    <p>Faça o login para ter acesso à sua conta.</p>
+                </div>
+
+                <form action="" method="POST">
+                    <p>
+                        <label for="">Email:</label>
+                        <input class="inputSign" type="text" name="email">
+                    </p>
+                    <p>
+                        <label for="">Senha:</label>
+                        <input class="inputSign" type="password" name="senha">
+                    </p>
+                    
+                    <p>
+                        <button id="signIn" type="submit">Entrar</button>
+                    </p>
+                </form>
+
+
 </body>
+
 </html>
